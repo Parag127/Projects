f4f9eb1b036f4c697383cf85fd2606665a9046dd
Parag Badgujar <badgujarparag419@gmail.com>
2026-01-10 02:58:32 +0530

    Array has be added successfully!!! Checkpoint

--- Quiz App/Quiz.js/Quiz.js
+++ Quiz App/Quiz.js/Quiz.js
@@ -5,59 +5,55 @@ document.addEventListener("DOMContentLoaded", () => {
   const restartBtn = document.getElementById("Restart-btn");
   const hiddenContent = document.getElementById("hidden-container");
   const question = document.getElementById("question");
+  const optionS = document.getElementById("option");
+  const heading = document.querySelector("h1");
 
-  let Questions = [
+  const QuesAns = [
     {
-      Question1: "Which is the largest mammal on Earth?",
-      options1: {
-        option1: "Megalodon",
-        option2: "Elephant",
-        option3: "Blue Whale",
-        option4: "Giraffe",
-      },
-    },
-    {
-      Question2: "In which sport is the term 'love' used?",
-      options2: {
-        option1: "Baseball",
-        option2: "Soccer",
-        option3: "Tennis",
-        option4: "Badminton",
-      },
-    },
-    {
-      Question3: "Which country is known as the ‘Land of the Rising Sun’?",
-      options3: {
-        option1: "Amsterdam",
-        option2: "China",
-        option3: "Japan",
-        option4: "New Zealand",
-      },
-    },
-    {
-      Question4: "What is the freezing point of water?",
-      options4: {
-        option1: "-5°C or 32°F",
-        option2: "0°C or 32°F",
-        option3: "0°C or 36°F",
-        option4: "-5°C or 36°F",
-      },
-    },
-  ];
-  const answers = [
-    {
-    Answer1 : Questions.options1.option3,
-    Answer2 : Questions.options2.option3,
-    Answer3 : Questions.options3.option3,
-    Answer4 : Questions.options4.option2,
-    }
-  ]
-
-  startBtn.addEventListener('click', () => {
-    renderOptions();
-  })
-
-  function renderOptions () {
-    hiddenContent.classList.remove('hidden')  
+    questions: "Which is the largest mammal on Earth?",
+    options: ["Megalodon", "Elephant", "Blue Whale", "Giraffe"],
+    answer: "Blue Whale"
+  },
+  {
+    questions: "In which sport is the term 'love' used?",
+    options: ["Baseball", "Soccer", "Tennis", "Badminton"],
+    answer: "Tennis"
+  },
+  {
+    questions: "Which country is known as the Land of the Rising Sun?",
+    options: ["Amsterdam", "China", "Japan", "New Zealand"],
+    answer: "Japan"
+  },
+  {
+    questions: "What is the freezing point of water?",
+    options: ["-5°C or 32°F", "0°C or 32°F", "0°C or 36°F", "-5°C or 36°F"],
+    answer: "0°C or 32°F"
   }
+];
+
+  const data = [];
+
+  startBtn.addEventListener("click", () => {
+    renderOptions();
+    displayQuesAns();
+  });
+
+  function displayQuesAns() {
+    QuesAns.forEach(ques => {
+      ques.questions;
+      ques.options;
+      ques.answer;
+    })
+
+    
+
+    data.push(QuesAns)
+  }
+
+  function renderOptions() {
+    startBtn.classList.add("hidden");
+    heading.classList.add("hidden");
+    hiddenContent.classList.remove("hidden");
+  }
+  console.log(data)
 });
