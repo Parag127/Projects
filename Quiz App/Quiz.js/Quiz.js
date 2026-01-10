c5c92bb95e7c137e1d1d1a69f9787de45f41bb31
Parag Badgujar <badgujarparag419@gmail.com>
2026-01-10 13:21:47 +0530

    Next button with proper function added successfully

--- Quiz App/Quiz.js/Quiz.js
+++ Quiz App/Quiz.js/Quiz.js
@@ -5,9 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const restartBtn = document.getElementById("Restart-btn");
   const hiddenContent = document.getElementById("hidden-container");
   const question = document.getElementById("question");
-  const optionS = document.getElementById("option");
+  const option1 = document.getElementById("option-1");
+  const option2 = document.getElementById("option-2");
+  const option3 = document.getElementById("option-3");
+  const option4 = document.getElementById("option-4");
   const heading = document.querySelector("h1");
-
+  let i = 0;
   const QuesAns = [
     {
     questions: "Which is the largest mammal on Earth?",
@@ -35,19 +38,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
   startBtn.addEventListener("click", () => {
     renderOptions();
+    storeQuesAns();
     displayQuesAns();
   });
 
-  function displayQuesAns() {
+  function storeQuesAns() {
     QuesAns.forEach(ques => {
       ques.questions;
       ques.options;
       ques.answer;
+      data.push(ques)
+    })
+    // console.log(QuesAns[0].questions)
+
+
+    question.innerHTML = `${QuesAns[i].questions}`
+    option1.innerHTML = `${QuesAns[i].options[0]}`
+    option2.innerHTML = `${QuesAns[i].options[1]}`
+    option3.innerHTML = `${QuesAns[i].options[2]}`
+    option4.innerHTML = `${QuesAns[i].options[3]}`
+  }
+
+  function displayQuesAns () {
+     nextBtn.addEventListener('click', () => {
+    
+    question.innerHTML = `${QuesAns[i].questions}`
+    option1.innerHTML = `${QuesAns[i].options[0]}`
+    option2.innerHTML = `${QuesAns[i].options[1]}`
+    option3.innerHTML = `${QuesAns[i].options[2]}`
+    option4.innerHTML = `${QuesAns[i].options[3]}`
+    i++;
     })
 
-    
-
-    data.push(QuesAns)
   }
 
   function renderOptions() {
